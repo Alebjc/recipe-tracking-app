8bce2051a5039fa834e5c21a9f7305917799a47d
Alexis <juniorbasarte@gmail.com>
2024-11-04 12:10:31 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [recipes, setRecipes] = useState(RecipeData);
+
+    // Add new recipe
+    const addRecipe = (newRecipe) => {
+        setRecipes([...recipes, { ...newRecipe, id: Date.now() }]);
+    };
+
+    // Delete existing recipe
+    const deleteRecipe = (id) => {
+        setRecipes(recipes.filter((recipe) => recipe.id !== id));
+    };
+
+    return (
+        <div className="App">
+            <header><h1>Delicious Food Recipes</h1></header>
+            <RecipeList recipes={recipes} onDelete={deleteRecipe} />
+            <RecipeCreate addRecipe={addRecipe} />
+        </div>
+    );
 }
 
 export default App;
